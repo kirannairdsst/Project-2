1d8c0deb997d2a064737a697f37cda44b59a38f7
kirannairdsst <31115950+kirannairdsst@users.noreply.github.com>
2017-11-10 14:20:36 -0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function hello() {
+  var response = prompt('Are you giving a presentation?', ' ');
+  if (response === "yes") {
+    alert('You deserve a 100%');
+  }
+  if (response === "no") {
+    alert('If you were giving a presentation you would get a 100%')
+  }
+}
